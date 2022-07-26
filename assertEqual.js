57ad353c2fa5178a61efb89e5fa971e9a89d719e
nickholmstyle <nick416holm@protonmail.com>
2022-07-26 03:27:31 +0000

    added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed:' + actual + ' !== ' + expected);
+  }
+};
+
+console.log(assertEqual("Sugar Shack", "Lighthouse Labs"));
+console.log(assertEqual(1, 1));
