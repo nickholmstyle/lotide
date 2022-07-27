3ce20e886969bfdc37907d410492366b3bab76c0
nickholmstyle <nick416holm@protonmail.com>
2022-07-27 18:02:46 +0000

    adding eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+
+
+
+const eqArrays = function(a, b) {
+  if (a === b) return true;
+  if (a === null || b === null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  
+  }
+  return true;
+};
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
