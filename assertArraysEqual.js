21e9f902c7ebc52863294b52147a90b531595483
nickholmstyle <nick416holm@protonmail.com>
2022-07-27 18:48:29 +0000

    adding asserArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(a, b) {
+  if (a === b) return true;
+  if (a === null || b === null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log("not equal arrays");
+  } else {
+    console.log("equal array")
+  } 
+   
+}
+
+const a = [1, 2, 3];
+const b = [3, 4, 5];
+//console.log(eqArrays(a, b));
+assertArraysEqual(a, b);
