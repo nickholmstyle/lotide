24d8b2c6fa2137cf277f092ce75f2d9121c190d6
nickholmstyle <nick416holm@protonmail.com>
2022-08-30 01:43:17 +0000

    built the index's export modules object with recently added functions

--- index.js
+++ index.js
@@ -8,16 +8,33 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const head   = require('./head');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
+const middle = require('./middle');
 const takeUntil = require('./takeUntil');
+const tail   = require('./tail');
 const without = require('./without');
 
+module.exports = {
+  assertArraysEqual: assertArraysEqual, 
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLett: countLett,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  takeUntil: takeUntil,
+  tail: tail,
+  without: without
+}
 
 
 
-
-
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
