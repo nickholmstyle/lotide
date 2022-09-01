e6ab55d9f0ea4c882d5305312704b5f194de1ea4
nickholmstyle <nick416holm@protonmail.com>
2022-09-01 01:42:03 +0000

    testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,15 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
